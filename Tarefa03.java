c780f362f7e1b0a415b77f6a4eecaf73b160301d
Thiago <thiagobarbosaribeiro@edu.uniube.br>
2024-03-05 17:08:22 -0300

    tarefa 3 concluída

---
+++ Tarefa03.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Tarefa03 {
+
+    static Scanner ler = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int n = 6;
+        char v[] = { 'R', 'O', 'N', 'E', 'D', 'A' };
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("v[%d] = %c\n", i, v[i]);
+        }
+
+        while (true) {
+            System.out.print("\nQual a posição de origem? ");
+            int i = ler.nextInt();
+
+            if (i >= 0 && i < 6) {
+                System.out.print("Qual a posição de destino? ");
+                int j = ler.nextInt();
+
+                if (j >= 0 && j < 6) {
+                    char aux = v[i];
+
+                    if (i < j) {
+                        for (int k = i; k < j; k++) {
+                            v[k] = v[k + 1];
+                        }
+                    } else {
+                        for (int k = i; k > j; k--) {
+                            v[k] = v[k - 1];
+                        }
+                    }
+
+                    v[j] = aux;
+                    break;
+
+                } else {
+                    System.out.println("Digite um valor entre 0 e 5.");
+                }
+            } else {
+                System.out.println("Digite um valor entre 0 e 5.");
+            }
+        }
+
+        System.out.printf("\nEstado final:\n");
+
+        for (char ch : v) {
+            System.out.printf("%c ", ch);
+        }
+    }
+}
