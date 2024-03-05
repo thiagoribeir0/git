abd439fc8aacb9137cd7619ac1629f3141ffd3de
Thiago <thiagobarbosaribeiro@edu.uniube.br>
2024-03-05 12:55:17 -0300

    tarefa 02 atualizada

--- Tarefa02.java
+++ Tarefa02.java
@@ -2,35 +2,41 @@ import java.util.Scanner;
 
 public class Tarefa02 {
 
-    Scanner ler = new Scanner(System.in);
+    static Scanner ler = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Tarefa02 programa = new Tarefa02();
-        programa.executar();
-    }
 
-    public void executar() {
         int n = 6;
         char v[] = { 'O', 'R', 'D', 'E', 'N', 'A' };
 
         for (int i = 0; i < n; i++) {
-            System.out.printf("v[%d] = %2c\n", i, v[i]);
+            System.out.printf("v[%d] = %c\n", i, v[i]);
         }
 
-        alterarValor();
-    }
-
-    public void alterarValor() {
-        int cont = 0;
-        do {
-            System.out.print("Qual a posição de origem? ");
+        while (true) {
+            System.out.print("\nQual a posição de origem? ");
             int i = ler.nextInt();
-            if (i >= 0 & i < 6) {
+
+            if (i >= 0 && i < 6) {
                 System.out.print("Qual a posição de destino? ");
                 int j = ler.nextInt();
+
+                if (j >= 0 && j < 6) {
+                    char aux = v[i];
+                    v[i] = v[j];
+                    v[j] = aux;
+                    break;
+                } else {
+                    System.out.println("Digite um valor entre 0 e 5.");
+                }
             } else {
                 System.out.println("Digite um valor entre 0 e 5.");
             }
-        } while (cont > 0 || cont < 6);
+        }
+
+        System.out.printf("\nEstado final:\n");
+        for (char ch : v) {
+            System.out.printf("%c ", ch);
+        }
     }
 }
